c28b2456aad0568c9105ea8e4daad1d97f13b697
bar rot <151166429+barrot9@users.noreply.github.com>
2024-04-27 20:17:37 +0300

    login-and-register

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import pg from "pg";
+import bcrypt from "bcrypt";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
@@ -26,7 +28,19 @@ let texts = [
     { post_id: 2, post_content: "test2" },
   ];
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/index", async (req, res) => {
     try{
         const result = await db.query("SELECT * FROM posts ORDER BY post_id ASC");
         texts = result.rows;
@@ -35,14 +49,75 @@ app.get("/", async (req, res) => {
     catch (err) {
         console.log(err);
     }
-    
+});
+
+app.get("/About", (req, res) => {
+  res.render("About.ejs" , { currentPage: 'About' });
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE username = $1", [
+      email,
+    ]);
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      //hashing the password and saving it in the database
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          await db.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2)",
+            [email, hash]
+          );
+          res.redirect("/index");
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const loginPassword = req.body.password;
+  try {
+    const result = await db.query("SELECT * FROM users WHERE username = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedHashedPassword = user.password;
+      //verifying the password
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) {
+          console.error("Error comparing passwords:", err);
+        } else {
+          if (result) {
+            res.redirect("/index");
+          } else {
+            res.send("Incorrect Password");
+          }
+        }
+      });
+    } else {
+      res.send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/add", async (req, res) => {
     const item = req.body.newItem;
     try {
       await db.query("INSERT INTO posts (post_content) VALUES ($1)", [item]);
-      res.redirect("/");
+      res.redirect("/index");
     } catch (err) {
       console.log(err);
     }
@@ -53,7 +128,7 @@ app.post("/edit", async (req, res) => {
     const id = req.body.updatedItemId;
     try {
       await db.query("UPDATE posts SET post_content = ($1) WHERE post_id = $2", [item, id]);
-      res.redirect("/");
+      res.redirect("/index");
     } catch (err) {
       console.log(err);
     }
@@ -63,16 +138,11 @@ app.post("/delete", async (req, res) => {
     const id = req.body.deleteItemId;
     try {
       await db.query("DELETE FROM posts WHERE post_id = $1", [id]);
-      res.redirect("/");
+      res.redirect("/index");
     } catch (err) {
       console.log(err);
     }
 });
-
-app.post("/About", (req, res) => {
-    res.render("About.ejs" , { currentPage: 'About' });
-});
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
